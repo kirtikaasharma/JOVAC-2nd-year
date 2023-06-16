04de170c683a0a2e88e5cee9a6c42b6a5f03d130
KIRTIKAA SHARMA <ks243d@gmail.com>
2023-06-16 14:04:10 +0530

    lectures
    lectures

---
+++ Lecture-6/introJS/app.js
@@ -0,0 +1,68 @@
+let vikas="haa bhaiya"
+let sagar='haa bhaiya'
+console.log(vikas)
+console.log(sagar)
+// cntrl+ C and cntrl+ V can be used to copy and paste a line in vs code n num of times.
+
+// naming conventions:
+// 1. normal
+let samarth=100;
+//2. camelcase(mostly used in development)
+let samarthVohra=10000;
+let myNameIsSamarthVohra=100000;
+// 3. snakecase
+let samarth_vohra=1;
+let my_name_is_samarth_vohra=true;
+//4. kabab case
+//let first-name='samarth'; // do words ke beech mein hyphen - (dash) hoga, which is invalid, hence, it is not used.(this maybe used for other languages tho in which it is considered valid.)
+//but mostly not used in source code.
+let firstName="kiki";
+let anotherName=firstName;//anotherName pointing to the same kiki, ie. it has a direct link to that same variable.
+console.log(firstName);
+console.log(anotherName);
+//concatenation
+let a="samrth";
+let b="vohra";
+console.log(a+" "+b);
+//there is nothing like a character in javascript. We have Single string containing only one character.
+
+let single='y'
+console.log(typeof(single))
+//numbers:
+let num1=10,num2=10.10,num3=-199;
+console.log(num1)
+console.log(num2)
+console.log(num3)
+//arithematic operations:
+let num4=num1*num2;
+console.log(num4);
+let ans= num1*num2/num3+num2-num4;//THis will be solves by the rule of PEMDAS(parenthesis,exponential,multiply,divide,add,subtract)
+console.log(ans)
+// ** -->power
+let ans2=(1*2-(3-4)+ 3**2/2);
+console.log(ans2); //7.5
+
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
