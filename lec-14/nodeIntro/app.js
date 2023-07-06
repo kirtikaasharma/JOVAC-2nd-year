976c43a9bfb1908f07c449ac1d4e466bb45b3096
KIRTIKAA SHARMA <ks243d@gmail.com>
2023-07-07 00:52:42 +0530

    lec14

---
+++ lec-14/nodeIntro/app.js
@@ -0,0 +1,7 @@
+console.log("hi from nodejs")
+
+
+let a = 10;
+let b = 20;
+
+console.log(a+b);
---
+++ lec-14/printTill-N/app.js
@@ -0,0 +1,9 @@
+let n = parseInt(process.argv.pop());
+// console.log(n);
+
+function naam(n){
+    for(let i=1 ; i<=n ; i++){
+        console.log(i);
+    }
+}
+naam(n);
